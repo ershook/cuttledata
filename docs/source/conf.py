14bc1b90541f41500a82b159365778779e666cbd
ershook <ericanshook@gmail.com>
2023-10-19 11:04:58 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'nbsphinx'
 ]
-
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
