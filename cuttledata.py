b52a33e0652c06f968f324f9135474f41ecabf4f
g-thomas-barlow <gtb2115@cumc.columbia.edu>
2024-01-11 17:05:30 -0500

    adding thomases image processing and parallel code

--- cuttledata.py
+++ cuttledata.py
@@ -91,7 +91,9 @@ class CuttleData:
         self.vgg = True
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        
+        self.DNG_path = prefix + 'cuttlefish/CUTTLEFISH_BEHAVIOR/2023_BEHAVIOR/E-ink_Tank/'+images_folder+'/DCIM/'
+
+
     
     def _get_mask_inds_vgg(self, image_no):
         new_size = (256,256)
@@ -357,25 +359,70 @@ class CuttleData:
             numpy.ndarray: The decoded mask as a NumPy array.
         """
         return mask_utils.decode(mask)
-        
+
+    def create_image_dictionary(self):
+        files = []
+        #print(self.DNG_path)
+        for root, dirs, filenames in os.walk(self.DNG_path):
+            dirs.sort()
+            for filename in sorted(filenames):
+                #print(filename)
+                if filename.lower().endswith(".dng") or filename.lower().endswith(".ARW") or filename.lower().endswith(".arw"):
+                    #print("found raw")
+                    files.append(os.path.join(root, filename))
+        file_dict = {}  # Define an empty dictionary
+
+        # Populate the dictionary with mappings
+        for i, file_path in enumerate(files, start=1):
+            filename = str(os.path.basename(file_path))
+            #print(file_number)
+            file_dict[filename] = i
+
+        return file_dict
 
 
-    def is_image_out_of_focus(self, image_no, threshold=100):
-        """
-        *** NOT TESTED -- USE AT YOUR OWN RISK (but please report back if you do) ***
-        Determines whether an image is out of focus based on its variance of Laplacian.
+    def generate_files(self, image_source, file_dict3, list_of_images, image_no):
 
-        Args:
-            image_no (int): Image number.
-            threshold (int): Variance threshold for determining focus. Default is 100.
-        """
-        image_file = f"{self.images_path}{str(image_no).zfill(5)}.tif"
-        image = cv2.imread(image_file)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        laplacian = cv2.Laplacian(gray, cv2.CV_64F)
-        variance = laplacian.var()
-        if variance < threshold:
-            print(f"Image {image_no} is out of focus.")
+        filename = next((k for k, v in file_dict3.items() if v == image_no), None)
+
+        for i in range(100, 110):
+            folder_name = f"{i}SIGMA"
+            folder_path = os.path.join(image_source, folder_name)
+
+            if os.path.isdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
+                if os.path.isfile(file_path):
+                    image_source = file_path
+
+        #print(image_source)
+        # Load the image once, and store it in this 'rgb' format
+        with rawpy.imread(image_source) as raw:
+            rgb = raw.postprocess(gamma=(50, 50), no_auto_bright=True, output_bps=8)
+
+        # if the image is taller than it is wide, rotate by 90 degrees
+        height, width, _ = rgb.shape
+        if height > width:
+            rgb = np.rot90(rgb)
+
+        # then look at what images we need to make. For each list of parameters . . .
+        filename = os.path.basename(image_source)
+        file_number = file_dict3.get(filename)
+
+        # apply the downsampling
+        resized_image = resize(rgb, list_of_images[0][0])
+        # apply the color correction, which involves the color crading and the color-> black and white conversion
+        processed = apply_color(resized_image, list_of_images[0][1], list_of_images[0][2])
+        # crop the image
+        cropped = crop_image(processed, list_of_images[0][3])
+        # save the image in the corresponding directory
+        return cropped.astype('uint8')
+
+
+    def process_file(self, file_dict2, list_of_images, image_no):
+        return self.generate_files(self.DNG_path, file_dict2, list_of_images, image_no)
+
+
+
         else:
             print(f"Image {image_no} is not out of focus.")
 
@@ -881,7 +928,18 @@ class CuttleData:
             plt.show()
         
         return pattern
-    
+
+    def is_image_out_of_focus(self, image_no, show_image=False):
+        #Thomas Lapv version
+        try:
+            masks = self.load_masks(image_no)
+            mantle = self.get_mantle_pattern(image_no)
+            mantle_256 = (mantle * 255).astype(np.uint8)
+            LapV_value = LAPV(mantle_256)
+            print("LAPV value:" + str(LapV_value))
+            return LapV_value
+        except:
+            print("no mask for image " + str(image_no))
 
     def get_mantle_pattern(self, image_no, show_image = False):
         """
@@ -1305,7 +1363,42 @@ class CuttleData:
         # Return the computed values
         return values
 
-    
+    def process_frame(self, func, ii):
+        try:
+            return func(ii)
+        except Exception as e:
+            print(f"Error processing frame {ii}: {e}")
+            return None  # Return None or a custom error indicator
+
+    def safe_process_frame(self, func, frame_number):
+        try:
+            return self.process_frame(func, frame_number)
+        except Exception as e:
+            print(f"Error processing frame {frame_number}: {e}")
+            return None  # Return None or a custom error indicator
+
+    def get_parallel_values_across_sessions(self, func):
+        path = self.storage_path + self.images_folder.replace('/', '') + '_' + func.__name__ + '.npy'
+        if os.path.exists(path):
+            return np.load(path)
+
+        print('values have not been computed before -- may take a bit of time.')
+
+        total_frames = self.num_frames()
+        print("Total Frames: " + str(total_frames))
+
+        with Pool(processes=16) as pool:  # Adjust the number of processes as needed
+            func_partial = partial(self.safe_process_frame, func)
+            results = pool.starmap(func_partial, [(i,) for i in range(1, total_frames)])
+
+        # Filter out None values or error indicators
+        self.values = [result for result in results if result is not None]
+
+        print(self.values)
+        print("Type of values: " + str(type(self.values)))
+
+        return self.values
+
     def mean_image(self, image_no):
         """
         Calculate the mean value of an image.
@@ -1498,7 +1591,94 @@ class CuttleData:
         return img
 
 
+def LAPV(img):
+    """Implements the Variance of Laplacian (LAP4) focus measure
+    operator. Measures the amount of edges present in the image.
 
+    :param img: the image the measure is applied to
+    :type img: numpy.ndarray
+    :returns: numpy.float32 -- the degree of focus
+    """
+    return np.std(cv2.Laplacian(img, cv2.CV_64F)) ** 2
+
+
+def LAPM(img):
+    """Implements the Modified Laplacian (LAP2) focus measure
+    operator. Measures the amount of edges present in the image.
+
+    :param img: the image the measure is applied to
+    :type img: numpy.ndarray
+    :returns: numpy.float32 -- the degree of focus
+    """
+    kernel = np.array([-1, 2, -1])
+    laplacianX = np.abs(cv2.filter2D(img, -1, kernel))
+    laplacianY = np.abs(cv2.filter2D(img, -1, kernel.T))
+    return np.mean(laplacianX + laplacianY)
+
+
+def TENG(img):
+    """Implements the Tenengrad (TENG) focus measure operator.
+    Based on the gradient of the image.
+
+    :param img: the image the measure is applied to
+    :type img: numpy.ndarray
+    :returns: numpy.float32 -- the degree of focus
+    """
+    gaussianX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+    gaussianY = cv2.Sobel(img, cv2.CV_64F, 1, 0)
+    return np.mean(gaussianX * gaussianX +
+                      gaussianY * gaussianY)
+
+
+def MLOG(img):
+    """Implements the MLOG focus measure algorithm.
+
+    :param img: the image the measure is applied to
+    :type img: numpy.ndarray
+    :returns: numpy.float32 -- the degree of focus
+    """
+    return np.max(cv2.convertScaleAbs(cv2.Laplacian(img, 3)))
+
+
+def pad_images_in_folder(folder_path):
+    max_width = 0
+    max_height = 0
+    image_paths = []
+
+    # Step 1: Find the max dimensions
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".tif"):
+            image_path = os.path.join(folder_path, filename)
+            image_paths.append(image_path)
+            print("checking: " + str(filename))
+            with Image.open(image_path) as img:
+                width, height = img.size
+                max_width = max(max_width, width)
+                max_height = max(max_height, height)
+
+    # Create new folder for padded images
+    padded_folder_path = folder_path + "_padded"
+    if not os.path.exists(padded_folder_path):
+        os.makedirs(padded_folder_path)
+
+    # Step 2: Pad each image
+    for image_path in image_paths:
+        print("Padding image:", image_path)
+        with Image.open(image_path) as img:
+            width, height = img.size
+            # Calculate padding sizes
+            left = (max_width - width) // 2
+            right = max_width - width - left
+            top = (max_height - height) // 2
+            bottom = max_height - height - top
+
+            # Create a new image with the desired size and black background
+            new_img = Image.new('RGB', (max_width, max_height), (0, 0, 0))
+            new_img.paste(img, (left, top))
+
+            # Save the padded image in the new folder
+            new_image_path = os.path.join(padded_folder_path, os.path.basename(image_path))
+            new_img.save(new_image_path)
 
     def gram_matrix(self, x):
         # From Laurent lab: https://gitlab.mpcdf.mpg.de/mpibr/laur/cuttlefish/texture-code/-/blob/main/featurespace.py?ref_type=heads
@@ -1554,6 +1734,167 @@ class CuttleData:
 
 
 
-        
+def resize(rgb, downsample_factor):
+    return cv2.resize(rgb, (0, 0), fx=downsample_factor, fy=downsample_factor, interpolation=cv2.INTER_AREA)
+
+
+def apply_color(resized, spectrum, grading):
+    # first apply the appropriate spectrum (raw, ocean sim, daylight sim)
+
+    if spectrum == "daylight":
+        #print("Daylight_sim")
+        resized=resized*256
+
+        popt_red = np.array([2.49977028e-01, 1.12525005e+00, 5.18763052e+03])
+        popt_green = np.array([5.76949661e-01, 1.04726996e+00, 1.47926918e+03])
+        popt_blue = np.array([7.88687346e+00, 8.20434666e-01, - 9.25428880e+03])
+
+        red_channel = resized[:, :, 0]
+        green_channel = resized[:, :, 1]
+        blue_channel = resized[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = nonlinear_func(red_channel, *popt_red)
+        green_output = nonlinear_func(green_channel, *popt_green)
+        blue_output = nonlinear_func(blue_channel, *popt_blue)
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+        colored = colored/256
+    if spectrum == "ocean":
+        #print("Ocean_sim")
+
+        red_channel = resized[:, :, 0]
+        green_channel = resized[:, :, 1]
+        blue_channel = resized[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = red_channel * (0.3 / 0.433333333) *2
+        green_output = green_channel * (0.4 / 0.433333333) *2
+        blue_output = blue_channel * (0.6 / 0.433333333) *2
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+
+    if spectrum == "raw":
+        # scale 12 bit to 16 bit
+        #colored = resized * (16 / 12)
+        colored = resized
+
+    if spectrum == "pop":
+        resized = resized*256
+
+        popt_red = np.array([2.49977028e-01, 1.12525005e+00, 5.18763052e+03])
+        popt_green = np.array([5.76949661e-01, 1.04726996e+00, 1.47926918e+03])
+        popt_blue = np.array([7.88687346e+00, 8.20434666e-01, - 9.25428880e+03])
+
+        red_channel = resized[:, :, 0]
+        green_channel = resized[:, :, 1]
+        blue_channel = resized[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = nonlinear_func(red_channel, *popt_red)
+        green_output = nonlinear_func(green_channel, *popt_green)
+        blue_output = nonlinear_func(blue_channel, *popt_blue)
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+
+        #colored = np.clip((2.1 * colored - 32000), 0, 65535)
+        #colored = np.clip((1.8 * colored - 24000), 0, 65535)
+        #colored = np.clip((1.7 * colored - 20000), 0, 65535)
+        colored = 1.6 * colored - 14000
+
+        red_channel = colored[:, :, 0]
+        green_channel = colored[:, :, 1]
+        blue_channel = colored[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = red_channel
+        green_output = green_channel - 2000
+        blue_output = blue_channel - 2600
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+        colored = np.float32(colored)
+        colored = colored/256
+        #print("pop")
+    if spectrum == "rawpop":
+        resized = resized*256
+
+        popt_red = np.array([2.49977028e-01, 1.12525005e+00, 5.18763052e+03])
+        popt_green = np.array([5.76949661e-01, 1.04726996e+00, 1.47926918e+03])
+        popt_blue = np.array([7.88687346e+00, 8.20434666e-01, - 9.25428880e+03])
+
+        red_channel = resized[:, :, 0]
+        green_channel = resized[:, :, 1]
+        blue_channel = resized[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = nonlinear_func(red_channel, *popt_red)
+        green_output = nonlinear_func(green_channel, *popt_green)
+        blue_output = nonlinear_func(blue_channel, *popt_blue)
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+
+        #colored = np.clip((2.1 * colored - 32000), 0, 65535)
+        #colored = np.clip((1.8 * colored - 24000), 0, 65535)
+        #colored = np.clip((1.7 * colored - 20000), 0, 65535)
+        #colored = 1.6 * colored - 20000
+        colored = 1.4 * colored - 25000
+        red_channel = colored[:, :, 0]
+        green_channel = colored[:, :, 1]
+        blue_channel = colored[:, :, 2]
+
+        # Apply the fitted functions to each channel
+        red_output = red_channel
+        green_output = green_channel + 4000
+        blue_output = blue_channel + 5200
+
+        red_output_clipped = np.clip(red_output, 0, 65535)
+        green_output_clipped = np.clip(green_output, 0, 65535)
+        blue_output_clipped = np.clip(blue_output, 0, 65535)
+
+        colored = np.stack((red_output_clipped, green_output_clipped, blue_output_clipped), axis=-1)
+        colored = np.float32(colored)
+        colored = colored/256
+        #print("pop")
+    if grading == "color":
+        graded = colored
+
+    if grading == "bw":
+        # graded = color.rgb2gray(colored)
+        graded = np.float32((colored * (8 / 12)))
+        graded = cv2.cvtColor(graded, cv2.COLOR_RGB2GRAY)
+
+    return graded
+
+def nonlinear_func(x, a, b, c):
+    return a * np.power(x, b) + c
+
+def crop_image(processed, crop_percent):
+    # Calculate crop size for both dimensions
+    crop_pixels_width = int(processed.shape[1] * crop_percent / 100)
+    #crop_pixels_height = int(processed.shape[0] * crop_percent / 200)
+    crop_pixels_height = int(processed.shape[0] * .01)
+    # Crop the image
+    cropped_image = processed[crop_pixels_height:-crop_pixels_height, crop_pixels_width:-crop_pixels_width]
+    return cropped_image
         
     
